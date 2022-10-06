f1c1e6a9ae6b249a5ae681655a763ed72bc3e6ba
okiriski29 <okihayday29@gmail.com>
2022-10-06 14:02:17 +0700

    upload project vue

---
+++ src/components/WomanShayna.vue
@@ -0,0 +1,106 @@
+<template>
+  <!-- Women Banner Section Begin -->
+  <section class="women-banner spad">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-lg-12 mt-5">
+          <carousel class="product-slider" :item="3" :nav="false" :autoplay="true" :dots="false">
+            <div class="product-item">
+              <div class="pi-pic">
+                <img src="img/celana_1.jpg" alt="" />
+                <ul>
+                  <li class="w-icon active">
+                    <a href="#"><i class="icon_bag_alt"></i></a>
+                  </li>
+                  <li class="quick-view"><router-link to="/product">+ Quick View</router-link></li>
+                </ul>
+              </div>
+              <div class="pi-text">
+                <div class="catagory-name">Coat</div>
+                <h5>Celana Panjang</h5>
+                <div class="product-price">
+                  Rp 199.000
+                  <span>Rp 399.000</span>
+                </div>
+              </div>
+            </div>
+            <div class="product-item">
+              <div class="pi-pic">
+                <img src="img/products/women-2.jpg" alt="" />
+                <ul>
+                  <li class="w-icon active">
+                    <a href="#"><i class="icon_bag_alt"></i></a>
+                  </li>
+                  <li class="quick-view"><a href="#">+ Quick View</a></li>
+                </ul>
+              </div>
+              <div class="pi-text">
+                <div class="catagory-name">Shoes</div>
+                <a href="#">
+                  <h5>Guangzhou sweater</h5>
+                </a>
+                <div class="product-price">$13.00</div>
+              </div>
+            </div>
+            <div class="product-item">
+              <div class="pi-pic">
+                <img src="img/products/women-3.jpg" alt="" />
+                <ul>
+                  <li class="w-icon active">
+                    <a href="#"><i class="icon_bag_alt"></i></a>
+                  </li>
+                  <li class="quick-view"><a href="#">+ Quick View</a></li>
+                </ul>
+              </div>
+              <div class="pi-text">
+                <div class="catagory-name">Towel</div>
+                <a href="#">
+                  <h5>Pure Pineapple</h5>
+                </a>
+                <div class="product-price">$34.00</div>
+              </div>
+            </div>
+            <div class="product-item">
+              <div class="pi-pic">
+                <img src="img/celana_2.jpg" alt="" />
+                <ul>
+                  <li class="w-icon active">
+                    <a href="#"><i class="icon_bag_alt"></i></a>
+                  </li>
+                  <li class="quick-view"><a href="#">+ Quick View</a></li>
+                  <li class="w-icon">
+                    <a href="#"><i class="fa fa-random"></i></a>
+                  </li>
+                </ul>
+              </div>
+              <div class="pi-text">
+                <div class="catagory-name">Towel</div>
+                <a href="#">
+                  <h5>Celana Panjang 2</h5>
+                </a>
+                <div class="product-price">Rp 249.000</div>
+              </div>
+            </div>
+          </carousel>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- Women Banner Section End -->
+</template>
+
+<script>
+import carousel from 'vue-owl-carousel';
+export default {
+  name: 'WomanShayna',
+  components: {
+    carousel,
+  },
+};
+</script>
+
+<style scoped>
+.product-item {
+  margin-right: 25px;
+}
+</style>
